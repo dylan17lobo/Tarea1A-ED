4c75ac067ac9d97db7c2a2c867728db616d2ce01
SamuelGM06 <samuegonza737@gmail.com>
2026-06-02 17:28:44 -0600

    Update Tarea1_Dylan_C5G526_Samuel_C5F560.java

--- Tarea1_Dylan_C5G526_Samuel_C5F560/src/tarea1_dylan_c5g526_samuel_c5f560/Tarea1_Dylan_C5G526_Samuel_C5F560.java
+++ Tarea1_Dylan_C5G526_Samuel_C5F560/src/tarea1_dylan_c5g526_samuel_c5f560/Tarea1_Dylan_C5G526_Samuel_C5F560.java
@@ -18,6 +18,7 @@ public class Tarea1_Dylan_C5G526_Samuel_C5F560 {
      */
     public static void main(String[] args) {
         Hashtable<String, String> inventario = new Hashtable<>();
+        System.err.println("AAAAA");
 
         System.out.println("=== 1. DESARROLLO DE METODOS DE HASHTABLE ===\n");
 
